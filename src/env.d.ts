c202f2bce103327e430b8cfe700f4cd288e845ca
Vikshan <103361575+vixshan@users.noreply.github.com>
2024-11-03 06:46:41 +0000

    initial commit

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+// @root/src/env.d.ts
+
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly CLIENT_ID: string;
+  readonly CLIENT_SECRET: string;
+  readonly BASE_URL: string;
+  readonly MONGO_CONNECTION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
